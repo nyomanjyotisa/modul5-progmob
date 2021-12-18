0d1f0285f51892f68aba6eaaa64cd982bc889038
nyomanjyotisa <jyotisa1616@gmail.com>
2021-12-18 20:52:29 +0800

    after hosting

--- app/src/main/java/com/example/praktikum/Constant.java
+++ app/src/main/java/com/example/praktikum/Constant.java
@@ -1,7 +1,7 @@
 package com.example.praktikum;
 
 public class Constant {
-    public static final String URL="http://10.0.2.2:8000/";
+    public static final String URL="https://progmob.seratusbali.xyz/";
     public static final String HOME=URL+"api";
     public static final String CHORDS=HOME+"/chords";
     public static final String ADD_CHORD=CHORDS+"/create";
